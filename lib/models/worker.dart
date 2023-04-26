5508586b8a2465bb8af54903dfb657f8d5411c83
dareen-obeid <201904878@bethlehem.edu>
2023-04-26 11:02:26 +0200

    review

--- lib/models/worker.dart
+++ lib/models/worker.dart
@@ -6,6 +6,7 @@ final email = worker?.email;
 
 class Worker {
   final String id;
+  final String uid;
   final String firstName;
   final String lastName;
   final String service;
@@ -15,9 +16,11 @@ class Worker {
   final String photoUrl;
   final String description;
   final Map<String, String> availability;
+  final List<String> mediaUrls;
 
   Worker({
     required this.id,
+    required this.uid,
     required this.firstName,
     required this.lastName,
     required this.service,
@@ -27,8 +30,8 @@ class Worker {
     required this.photoUrl,
     required this.description,
     required this.availability,
+    required this.mediaUrls,
   });
-
   factory Worker.fromSnapshot(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
 
@@ -43,23 +46,26 @@ class Worker {
       photoUrl: data['photoUrl'] ?? '',
       description: data['description'] ?? '',
       availability: Map<String, String>.from(data['availability'] ?? {}),
+      mediaUrls:
+          data['mediaUrls'] != null ? List<String>.from(data['mediaUrls']) : [],
+      uid: data['uid'] ?? '',
     );
   }
 
-  static Future<Worker?> getWorkerByEmail(String email) async {
-    final querySnapshot = await FirebaseFirestore.instance
-        .collection('workers')
-        .where('email', isEqualTo: email)
-        .limit(1)
-        .get();
+  // static Future<Worker?> getWorkerByEmail(String email) async {
+  //   final querySnapshot = await FirebaseFirestore.instance
+  //       .collection('workers')
+  //       .where('email', isEqualTo: email)
+  //       .limit(1)
+  //       .get();
 
-    if (querySnapshot.docs.isEmpty) {
-      return null;
-    }
+  //   if (querySnapshot.docs.isEmpty) {
+  //     return null;
+  //   }
 
-    final doc = querySnapshot.docs.first;
-    return Worker.fromSnapshot(doc);
-  }
+  //   final doc = querySnapshot.docs.first;
+  //   return Worker.fromSnapshot(doc);
+  // }
   // read one worker
 // static Future<Worker?> getWorkerByEmail(String email) async {
 //   final workerRef = FirebaseFirestore.instance.collection('workers').doc(email);
@@ -86,56 +92,56 @@ class Worker {
 // }
 
   // read workers
-  static Future<List<Worker>> getWorkers() async {
-    final workersRef = FirebaseFirestore.instance.collection('workers');
-    final workersSnapshots = await workersRef.get();
+  // static Future<List<Worker>> getWorkers() async {
+  //   final workersRef = FirebaseFirestore.instance.collection('workers');
+  //   final workersSnapshots = await workersRef.get();
 
-    final workers = <Worker>[];
-    for (final workerSnapshot in workersSnapshots.docs) {
-      final worker = Worker.fromSnapshot(workerSnapshot);
-      workers.add(worker);
-    }
+  //   final workers = <Worker>[];
+  //   for (final workerSnapshot in workersSnapshots.docs) {
+  //     final worker = Worker.fromSnapshot(workerSnapshot);
+  //     workers.add(worker);
+  //   }
 
-    return workers;
-  }
+  //   return workers;
+  // }
 
-    // update method
-  static Future<void> update(
-    String id, {
-    String? firstName,
-    String? lastName,
-    String? service,
-    String? city,
-    String? phone,
-    String? email,
-    String? photoUrl,
-    String? description,
-    Map<String, String>? availability,
-  }) async {
-    try {
-      // Get the document reference for the worker document
-      final docRef = FirebaseFirestore.instance.collection('workers').doc(id);
+  // update method
+  // static Future<void> update(
+  //   String id, {
+  //   String? firstName,
+  //   String? lastName,
+  //   String? service,
+  //   String? city,
+  //   String? phone,
+  //   String? email,
+  //   String? photoUrl,
+  //   String? description,
+  //   Map<String, String>? availability,
+  // }) async {
+  //   try {
+  //     // Get the document reference for the worker document
+  //     final docRef = FirebaseFirestore.instance.collection('workers').doc(id);
 
-      // Create a map of the updated data
-      final updateData = {
-        if (firstName != null) 'firstName': firstName,
-        if (lastName != null) 'lastName': lastName,
-        if (service != null) 'service': service,
-        if (city != null) 'city': city,
-        if (phone != null) 'phone': phone,
-        if (email != null) 'email': email,
-        if (photoUrl != null) 'photoUrl': photoUrl,
-        if (description != null) 'description': description,
-        if (availability != null) 'availability': availability,
-      };
+  //     // Create a map of the updated data
+  //     final updateData = {
+  //       if (firstName != null) 'firstName': firstName,
+  //       if (lastName != null) 'lastName': lastName,
+  //       if (service != null) 'service': service,
+  //       if (city != null) 'city': city,
+  //       if (phone != null) 'phone': phone,
+  //       if (email != null) 'email': email,
+  //       if (photoUrl != null) 'photoUrl': photoUrl,
+  //       if (description != null) 'description': description,
+  //       if (availability != null) 'availability': availability,
+  //     };
 
-      // Update the document in Cloud Firestore
-      await docRef.update(updateData);
-    } catch (e) {
-      print('Error updating worker document: $e');
-      rethrow;
-    }
-  }
+  //     // Update the document in Cloud Firestore
+  //     await docRef.update(updateData);
+  //   } catch (e) {
+  //     print('Error updating worker document: $e');
+  //     rethrow;
+  //   }
+  // }
 
   //add worker
   static Future<void> postWorker({
@@ -153,36 +159,29 @@ class Worker {
     final FirebaseFirestore _firestore = FirebaseFirestore.instance;
     final User? user = FirebaseAuth.instance.currentUser;
 
-    await workerRef
-        .add({
-          'firstName': firstName,
-          'lastName': lastName,
-          'service': service,
-          'city': city,
-          'phone': phone,
-          'email': email,
-          'photoUrl': photoUrl,
-          'description': description,
-          'availability': availability,
-          'uid': user!.uid,
-        });
-        
-        
+    await workerRef.add({
+      'firstName': firstName,
+      'lastName': lastName,
+      'service': service,
+      'city': city,
+      'phone': phone,
+      'email': email,
+      'photoUrl': photoUrl,
+      'description': description,
+      'availability': availability,
+      'uid': user!.uid,
+    });
   }
 
-
-static Future<Map<String, dynamic>> getWorkerByUid(String uid) async {
-  final workerRef = FirebaseFirestore.instance.collection('workers');
-  final workerSnapshot = await workerRef.where('uid', isEqualTo: uid).get();
-  if (workerSnapshot.docs.isEmpty) {
-    return {}; // return an empty map if the worker is not found
-  } else {
-    final workerData = workerSnapshot.docs.first.data();
-    workerData['id'] = workerSnapshot.docs.first.id; // add the worker document id to the map
-    return workerData; // return the worker data as a map
-  }
-}
-
-
-  
+// static Future<Map<String, dynamic>> getWorkerByUid(String uid) async {
+//   final workerRef = FirebaseFirestore.instance.collection('workers');
+//   final workerSnapshot = await workerRef.where('uid', isEqualTo: uid).get();
+//   if (workerSnapshot.docs.isEmpty) {
+//     return {}; // return an empty map if the worker is not found
+//   } else {
+//     final workerData = workerSnapshot.docs.first.data();
+//     workerData['id'] = workerSnapshot.docs.first.id; // add the worker document id to the map
+//     return workerData; // return the worker data as a map
+//   }
+// }
 }
