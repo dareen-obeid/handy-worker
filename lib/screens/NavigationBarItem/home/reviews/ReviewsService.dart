63dcda72b1a7674c6cf1b789fa6764c5daddfc83
dareen-obeid <dareen.3beid1@gmail.com>
2023-05-31 13:38:04 +0300

    review service

--- lib/screens/NavigationBarItem/home/reviews/ReviewsService.dart
+++ lib/screens/NavigationBarItem/home/reviews/ReviewsService.dart
@@ -6,8 +6,32 @@ class ReviewsService {
   final CollectionReference reviewsCollection =
       FirebaseFirestore.instance.collection('reviews');
 
-  Future<void> addReview(String workerId, String userId, String text,
-      double rating, DateTime timestamp) async {
+  Future<List<Review>> getReviewsForWorker(String workerId) async {
+    QuerySnapshot snapshot = await reviewsCollection
+        .where('workerId', isEqualTo: workerId)
+        .get();
+
+    List<Review> reviews = snapshot.docs.map((doc) {
+      return Review(
+        id: doc.id,
+        userId: doc['userId'],
+        workerId: doc['workerId'],
+        text: doc['text'],
+        rating: (doc['rating'] ?? 0.0).toDouble(),
+        timestamp: (doc['timestamp'] as Timestamp).toDate(),
+      );
+    }).toList();
+
+    return reviews;
+  }
+
+  Future<void> addReview(
+    String workerId,
+    String userId,
+    String text,
+    double rating,
+    DateTime timestamp,
+  ) async {
     await reviewsCollection.add({
       'workerId': workerId,
       'userId': userId,
@@ -15,21 +39,5 @@ class ReviewsService {
       'rating': rating,
       'timestamp': timestamp,
     });
-  }
-
-  Stream<List<Review>> getReviewsForWorker(String workerId) {
-    return reviewsCollection
-        .where('workerId', isEqualTo: workerId)
-        .snapshots()
-        .map((snapshot) => snapshot.docs
-            .map((doc) => Review(
-                  id: doc.id,
-                  userId: doc['userId'],
-                  workerId: doc['workerId'],
-                  text: doc['text'],
-                  rating: doc['rating'],
-                  timestamp: doc['timestamp'].toDate(),
-                ))
-            .toList());
   }
 }
