a68e125709680cd989ebc5392704a8d21befd718
dareen-obeid <dareen.3beid1@gmail.com>
2023-06-20 19:42:17 +0300

    noti

--- lib/screens/NavigationBarItem/notification.dart
+++ lib/screens/NavigationBarItem/notification.dart
@@ -5,27 +5,72 @@ class NotificationPage extends StatefulWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _NotificationPageState createState() => _NotificationPageState();
 }
 
 class _NotificationPageState extends State<NotificationPage> {
+  List<String> services = [
+    "All",
+    "Electrical work",
+    "Plumbing",
+    "Painting",
+    "Blacksmithing",
+    "Welding",
+    "Glasswork",
+    "Appliance repair",
+    "Tiling",
+    "Carpentry",
+    "HVAC",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Center(
-          child: Text("noti"),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            final FirebaseAuth _auth = FirebaseAuth.instance;
-            final User? user = _auth.currentUser;
-            print(user);
-          },
-          child: const Text('Press me'),
-        ),
+                  const SizedBox(height: 20),
+
+        Text("Notification")
+        // Container(
+        //   height: 40,
+        //   child: ListView.builder(
+        //     scrollDirection: Axis.horizontal,
+        //     itemCount: services.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       bool isSelected = (index == 0); // Select the "All" option by default
+
+        //       return Container(
+        //         width: 106,
+        //         margin: const EdgeInsets.only(left: 4, right: 4),
+        //         decoration: BoxDecoration(
+        //           color: isSelected ? const Color(0xFF00ABB3) : Colors.white,
+        //           borderRadius: BorderRadius.circular(20),
+        //           border: Border.all(
+        //             color: const Color(0xFF00ABB3),
+        //             width: 2,
+        //           ),
+        //         ),
+        //         child: Center(
+        //           child: Text(
+        //             services[index],
+        //             style: TextStyle(
+        //               fontSize: 12,
+        //               color: isSelected ? Colors.white : const Color(0xFF00ABB3),
+        //               fontWeight: FontWeight.bold,
+        //             ),
+        //           ),
+        //         ),
+        //       );
+        //     },
+        //   ),
+        // ),
+        // // const SizedBox(height: 8),
+        // const Text(
+        //   "Top Ten",
+        //   style: TextStyle(
+        //     fontSize: 16,
+        //     fontWeight: FontWeight.bold,
+        //   ),
+        // ),
       ],
     );
   }
